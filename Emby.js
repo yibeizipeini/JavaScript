25719872ac2ade170b3d1bd55dbf781d7281e25d
yibeizipeini <113541044+yibeizipeini@users.noreply.github.com>
2022-12-05 16:54:56 +0800

    Create Emby.js

--- Emby.js
+++ Emby.js
@@ -1,6 +1,3 @@
-// Variables used by Scriptable.
-// These must be at the very top of the file. Do not edit.
-// icon-color: blue; icon-glyph: magic;
 if ($request.url.indexOf('mb3admin.com/admin/service/registration/validateDevice') != -1) {
     if($response.status!=200){
         $notification.post("Emby Premiere 已激活", "", "");
