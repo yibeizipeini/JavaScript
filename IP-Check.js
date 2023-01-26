4e5f9641e06124a58fc9cda4bcf3848d3661b850
yibeizipeini <113541044+yibeizipeini@users.noreply.github.com>
2023-01-26 21:45:12 +0800

    Update IP-Check.js

--- IP-Check.js
+++ IP-Check.js
@@ -1,6 +1,3 @@
-// Variables used by Scriptable.
-// These must be at the very top of the file. Do not edit.
-// icon-color: pink; icon-glyph: magic;
 let url = "http://ip-api.com/json"
 
 $httpClient.get(url, function(error, response, data){
