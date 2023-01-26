0640c59acdaaa300a524feb130e1b8857f4ec371
yibeizipeini <113541044+yibeizipeini@users.noreply.github.com>
2023-01-26 21:44:05 +0800

    Add files via upload

---
+++ IP-Check.js
@@ -0,0 +1,31 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: pink; icon-glyph: magic;
+let url = "http://ip-api.com/json"
+
+$httpClient.get(url, function(error, response, data){
+    let jsonData = JSON.parse(data)
+    let country = jsonData.country
+    let emoji = getFlagEmoji(jsonData.countryCode)
+    let city = jsonData.city
+    let isp = jsonData.isp
+    let ip = jsonData.query
+  body = {
+    title: "节点信息",
+    content: `IP信息：${ip}\n运营商：${isp}\n所在地：${emoji}${country} - ${city}`,
+    icon: "globe.asia.australia.fill"
+  }
+  $done(body);
+});
+
+function getFlagEmoji(countryCode) {
+      if (countryCode.toUpperCase() == 'TW') {
+    countryCode = 'CN'
+  }
+  const codePoints = countryCode
+    .toUpperCase()
+    .split('')
+    .map(char => 127397 + char.charCodeAt())
+  return String.fromCodePoint(...codePoints)
+
+}
